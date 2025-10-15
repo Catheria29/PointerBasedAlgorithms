667fff997cdee64335a524a6ebf2fd497bda3d64
Catheria29 <100140576+Catheria29@users.noreply.github.com>
2025-10-15 06:12:06 +0300

    Create maxAndMin.c

---
+++ maxAndMin.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+void swap(int *,int *);
+void maxAndMin(int[],int*,int*,int);
+int main()
+{//swap
+// 	int a = 5,b = 7;
+// 	printf("a is %d and b is %d", a,b);
+	// int temp = a;
+	// a = b;
+	// b = temp;
+//     swap(&a,&b);
+// 	printf("\na is %d and b is %d", a,b);
+	
+//max and min
+
+	int arr[] = {1,5,3,7,87,44,322,6,8,9};
+	int max,min;
+	int size = sizeof(arr)/sizeof(arr[0]);
+	maxAndMin(arr,&max,&min,size);
+	printf("\n%d",max);
+	return 0;
+}
+
+void swap(int *n1, int *n2) {
+	int temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+}
+
+void maxAndMin(int arr[],int *max, int *min,int size){
+   
+   *max = arr[0];
+   *min = arr[0];
+   for(int i=0; i < size; i++){
+       if(*max < arr[i]){
+           *max = arr[i];
+       }
+       if(*min > arr[i]){
+           *min = arr[i];
+       }
+   }
+   printf("size is %d\n",size);
+   printf("max = %d and min = %d",*max,*min);
+}
