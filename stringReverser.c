d5af689148464154d39a7bf9f4ae5ccb4e1128fa
Catheria29 <100140576+Catheria29@users.noreply.github.com>
2025-10-15 15:54:33 +0300

    Create stringReverser.c
    Used pointer and malloc() to reverse an array of character in C

---
+++ stringReverser.c
@@ -0,0 +1,28 @@
+//reversing a string;
+#include <stdio.h>
+#include <stdlib.h>
+char* stringReverser(char str[],int size){
+    char *p = malloc(size + 1);
+    int cnt = 0;
+    for(int i = size -1; i >= 0; i--){
+        *(p+i) = str[i];
+        
+        cnt++;
+        }
+        *(p+size) = '\0';
+        
+    return p;
+       
+}
+
+int main()
+{
+    char str[] = "Hello World";
+    int size = sizeof(str) / sizeof(str[0]);
+    
+    char *newStr = stringReverser(str,12);
+    for (int i = 0; i < size; i++) {
+        printf("%c\n", newStr[i]);
+    }
+    return 0;
+}
